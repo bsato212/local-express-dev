4d102703b00ddb309dc665a0b5edb5ea7f284e27
Evan Poon <evnpoon@gmail.com>
2021-07-26 16:27:01 -0400

    Assignment 13

--- src/app.js
+++ src/app.js
@@ -4,9 +4,15 @@ const express = require('express');
 // Creates an Express App
 var app = express();
 app.use('/static', express.static(__dirname + "/static-assets"));
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-  res.send('This is main');
+  res.render('index', {
+    title: 'Assignment 13',
+    header1: 'View engine HTML',
+    p1: 'index.pug'
+  });
 });
 
 // Exports the Express App
