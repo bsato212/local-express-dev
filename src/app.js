84af52601766406966587d571592bb46262cfa76
Evan Poon <evnpoon@gmail.com>
2021-07-26 10:46:50 -0400

    Assignment 12

--- src/app.js
+++ src/app.js
@@ -2,33 +2,11 @@
 const express = require('express');
 
 // Creates an Express App
-const app = express();
-// Enables JSON and URL-Encoded parameter parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+var app = express();
+app.use('/static', express.static(__dirname + "/static-assets"));
 
-// Creates a POST handler for /
-app.post('/', function (req, res) {
-  // Logs query string parameters and headers
-  console.log(`Query string parameters [${JSON.stringify(req.body, null, '  ')}]`);
-  console.log(`Request headers [${JSON.stringify(req.headers, null, '  ')}]`);
-  
-  let name = '';
-  if(req.body.name != '') {
-    name = req.body.name;
-  } else {
-    name = 'World';
-  }
-
-  res.status(200);
-  res.send(`Hello ${name}!`);
-});
-
-// Error handler
-app.use(function (error, req, res, next) {
-  console.error(error);
-  res.status(500);
-  res.send('Error');
+app.get('/', (req, res) => {
+  res.send('This is main');
 });
 
 // Exports the Express App
---
+++ src/static-assets/testjs.js
@@ -0,0 +1,1 @@
+console.log('Welcome to testjs.js');
