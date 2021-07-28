7db0061d5ca99950e3f4e5a9f1a09e88e7796881
Evan Poon <evnpoon@gmail.com>
2021-07-28 16:06:10 -0400

    Assignment 12- Bruno edits

--- src/app.js
+++ src/app.js
@@ -5,9 +5,5 @@ const express = require('express');
 var app = express();
 app.use(express.static(__dirname + "/static-assets"));
 
-app.get('/', (req, res) => {
-  res.send('This is main');
-});
-
 // Exports the Express App
 module.exports = app;
